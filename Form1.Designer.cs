c87e02f6b3587c91c06f82ccfcdac9f92b5e8201
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2021-11-09 21:09:00 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ServerConfigForm_P5_20190140015
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelKet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SERVER";
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(75, 150);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(75, 23);
+            this.btnOn.TabIndex = 1;
+            this.btnOn.Text = "ON";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(187, 150);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(75, 23);
+            this.btnOff.TabIndex = 2;
+            this.btnOff.Text = "OFF";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(140, 79);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(61, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "Server : Off";
+            // 
+            // labelKet
+            // 
+            this.labelKet.AutoSize = true;
+            this.labelKet.Location = new System.Drawing.Point(140, 107);
+            this.labelKet.Name = "labelKet";
+            this.labelKet.Size = new System.Drawing.Size(136, 13);
+            this.labelKet.TabIndex = 4;
+            this.labelKet.Text = "Click ON to enabling server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 291);
+            this.Controls.Add(this.labelKet);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelKet;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using ServiceMtk_P1_20190140015;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_20190140015
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost Host; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+            btnOn.Enabled = false;
+            btnOff.Enabled = true;
+            Host.Open();   
+            labelStatus.Text = "Server : ON";
+            labelKet.Text = "Click OFF to disabling server";
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            btnOn.Enabled  = true;
+            btnOff.Enabled = false;
+            Host.Close();
+            labelStatus.Text = "Server : OFF";
+            labelKet.Text = "Click ON to enabling server";
+        }
+    }
+}
